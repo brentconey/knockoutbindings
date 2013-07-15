c8410c9553d103b95cdd5eb36906bedd02e5cace
Brent Coney <brentconey@gmail.com>
2013-07-15 17:30:44 -0500

    Slide basic example

---
+++ binding-slide.js
@@ -0,0 +1,9 @@
+﻿ko.bindingHandlers.slideIn = {
+    init: function (element, valueAccessor) {
+        $(element).toggle(ko.utils.unwrapObservable(valueAccessor()));
+    },
+    update: function (element, valueAccessor) {
+        var value = valueAccessor();
+        ko.utils.unwrapObservable(value) ? $(element).slideDown() : $(element).slideUp();
+    }
+};
