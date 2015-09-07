40f2ad096f9adb12cbc51707e025cc390091c7cd
Brent Coney <brentconey@gmail.com>
2015-09-07 14:12:24 -0500

    updated the inline edit for selecting text and updating the value

--- binding-inlineedit.js
+++ binding-inlineedit.js
@@ -2,8 +2,25 @@ ko.bindingHandlers.inlineEdit = {
     init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
         var value = valueAccessor();
         $(element).text(ko.unwrap(value));
-        $(element).on("input", function(e){
+        $(element).on("input", function (e) {
             value($(this).text());
         });
-    }
+		$(element).on("focus click", function (e) {
+			var sel, range;
+			if (window.getSelection && document.createRange) {
+				range = document.createRange();
+				range.selectNodeContents(element);
+				sel = window.getSelection();
+				sel.removeAllRanges();
+				sel.addRange(range);
+			} else if (document.body.createTextRange) {
+				range = document.body.createTextRange();
+				range.moveToElementText(element);
+				range.select();
+			}
+		});
+    },
+	update: function(element, valueAccessor){
+		$(element).text(ko.unwrap(valueAccessor()));
+	}
 };
