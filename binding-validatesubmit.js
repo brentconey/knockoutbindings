d88bfef90a0305a1fa48f203c702a8503da4dbbf
Brent Coney <brentconey@gmail.com>
2016-01-21 09:18:58 -0600

    Updated with some validation text.

--- binding-validatesubmit.js
+++ binding-validatesubmit.js
@@ -1,9 +1,10 @@
-﻿//Binding by Brent Coney, 2015
+//Binding by Brent Coney, 2015
 ko.bindingHandlers.validateSubmit = {
     init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
         if (typeof valueAccessor() != 'function') {
             throw new Error('The value for a validate submit binding must be a function');
         }
+        var validationMessageTemplate = '<span class="field-validation-error">{0}</span>';
         var $form = $(element);
         //disable default behavior
         $form.attr('novalidate', '');
@@ -12,31 +13,66 @@ ko.bindingHandlers.validateSubmit = {
             var validationErrorCount = 0;
             var deferredValidationCalls = [];
             var $allInputs = $form.find('input, textarea, select').not(':input[type=submit], :input[type=button], :input[type=reset]');
-            $allInputs.removeClass('error');
+            $allInputs.removeClass('form-error');
             for (var i = 0; i <= ($allInputs.length - 1) ; i++) {
                 var $input = $($allInputs[i]);
+                $input.next(".field-validation-error").remove();
+                var validationText = $input.data("validation-message") || "Invalid input";
                 if (typeof $input.attr('required') !== typeof undefined) {
                     //input has the required field.
                     if (!$input.val() || $input.val() === '') {
                         validationErrorCount++;
-                        $input.addClass('error');
+                        $input.addClass('form-error');
+                        if ($input.next(".field-validation-error").length === 0) {
+                            $input.after(validationMessageTemplate.replace("{0}", validationText));
+                        }
                     }
                 }
                 if (typeof $input.attr('min') !== typeof undefined) {
                     //input has min field
-                    if ($input.val().length < $input.attr('min')) {
+                    if ($input.val() < $input.attr('min')) {
                         validationErrorCount++;
-                        if (!$input.hasClass('error')) {
-                            $input.addClass('error');
+                        if (!$input.hasClass('form-error')) {
+                            $input.addClass('form-error');
+                            if ($input.next(".field-validation-error").length === 0) {
+                                $input.after(validationMessageTemplate.replace("{0}", validationText));
+                            }
                         }
                     }
                 }
                 if (typeof $input.attr('max') !== typeof undefined) {
                     //input has max field
-                    if ($input.val().length > $input.attr('max')) {
+                    if ($input.val() > $input.attr('max')) {
                         validationErrorCount++;
-                        if (!$input.hasClass('error')) {
-                            $input.addClass('error');
+                        if (!$input.hasClass('form-error')) {
+                            $input.addClass('form-error');
+                            if ($input.next(".field-validation-error").length === 0) {
+                                $input.after(validationMessageTemplate.replace("{0}", validationText));
+                            }
+                        }
+                    }
+                }
+                var lengthMax = $input.data("val-length-max");
+                if (lengthMax) {
+                    if ($input.val().length > parseInt(lengthMax)) {
+                        validationErrorCount++;
+                        if (!$input.hasClass('form-error')) {
+                            $input.addClass('form-error');
+                            if ($input.next(".field-validation-error").length === 0) {
+                                $input.after(validationMessageTemplate.replace("{0}", validationText));
+                            }
+                        }
+                    }
+                }
+                var lengthMin = $input.data("val-length-min");
+                if (lengthMin) {
+                    if ($input.val().length < parseInt(lengthMin)) {
+                        validationErrorCount++;
+                        if (!$input.hasClass('form-error')) {
+                            $input.addClass('form-error');
+                            if ($input.next(".field-validation-error").length === 0) {
+                                $input.after(validationMessageTemplate.replace("{0}", validationText));
+                            }
                         }
                     }
                 }
